9d95e2b27182db70af9e3c02e833178c3dbdbcf6
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-06-02 20:55:20 +0100

    Added route to generate QR code

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('qrcode', function () {
+    return QrCode::size(300)->generate('A basic example of QR code!');
+});
+
+Route::get('qrcode-with-color', function () {
+    return QrCode::size(300)
+                    ->backgroundColor(255,55,0)
+                    ->generate('A simple example of QR code');
+});
+
+Route::get('qr-code-g', function () {
+    return QrCode::size(500)
+              ->format('png')
+              ->generate('ItSolutionStuff.com', public_path('images/qrcode.png'));      
+});
