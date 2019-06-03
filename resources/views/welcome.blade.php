cd606446857af06211f468e0660b61599b3318ae
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-06-03 05:22:20 +0100

    Added route to generate QR code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    QR Code Generator
                 </div>
 
                 <div class="links">
